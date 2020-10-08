0eaffa152f006517986cb5f8d8742898a6137c59
Ulrich Barnstedt <ulrich.barnstedt@gmail.com>
2020-10-08 17:27:54 +0200

    Celluar Test

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Cellular from 'expo-cellular';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text> {Cellular.carrier} {"<->"} {Cellular.isoCountryCode} : {Cellular.mobileCountryCode} : {Cellular.mobileNetworkCode}</Text>
       <StatusBar style="auto" />
     </View>
   );
