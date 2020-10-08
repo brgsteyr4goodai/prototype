8a3b3f1b5a9222ad455d53a162197b32c6978429
OliverKovacs <oliver.kovacs.dev@gmail.com>
2020-10-08 18:56:44 +0200

    Restructured directory

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Cellular from 'expo-cellular';
+import Test from "./app/components/Test.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> {Cellular.carrier} {"<->"} {Cellular.isoCountryCode} : {Cellular.mobileCountryCode} : {Cellular.mobileNetworkCode}</Text>
+      <Test />
       <StatusBar style="auto" />
     </View>
   );
---
+++ app/components/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from 'react-native';
+
+function Test(props) {
+    return (
+        <Text>Import Worked</Text>
+    );
+}
+
+export default Test;
